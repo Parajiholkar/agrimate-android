e80c45aeeede62f8acf9bd6e5d359fb95c54dc38
Shubham5250 <shubhampaw312@gmail.com>
2023-04-23 01:57:58 +0530

    login-signup layout done

---
+++ app/src/main/java/com/example/agrimate/User.java
@@ -0,0 +1,17 @@
+package com.example.agrimate;
+
+public class User {
+
+    public String user_name, user_phone, user_email;
+
+    public User(){
+
+    }
+
+    public User(String user_name, String user_phone, String user_email){
+        this.user_name = user_name;
+        this.user_phone = user_phone;
+        this.user_email = user_email;
+    }
+
+}
